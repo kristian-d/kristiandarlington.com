48150f3396103362d5c851745bdd65fe31a77446
Kristian Darlington <kristian.f.darlington@gmail.com>
2020-05-11 20:20:03 -0700

    added url scheme checking for http redirects to https

--- web/router.go
+++ web/router.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/kristian-d/kristiandarlington.com/config"
 	"github.com/kristian-d/kristiandarlington.com/web/ui"
@@ -14,6 +15,22 @@ type route struct {
 }
 
 type routes []route
+
+/*func httpsRedirectMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		proto := req.Header.Get("x-forwarded-proto")
+		if proto == "http" || proto == "HTTP" {
+			http.Redirect(res, req, fmt.Sprintf("https://%s%s", req.Host, req.URL), http.StatusPermanentRedirect)
+			return
+		}
+
+		next.ServeHTTP(res, req)
+	})
+}*/
+
+func httpsRedirectHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, fmt.Sprintf("https://%s%s", r.Host, r.URL), http.StatusPermanentRedirect)
+}
 
 func NewRouter(cfg *config.Config) *mux.Router {
 	var myRoutes = routes{
@@ -56,11 +73,12 @@ func NewRouter(cfg *config.Config) *mux.Router {
 		router.
 			Methods(route.method).
 			Path(route.endpoint).
-			Handler(route.handler)
+			Handler(route.handler).
+			Schemes("https")
 	}
 
 	// for serving static files
-	router.PathPrefix("/static/").Handler(http.FileServer(ui.Assets))
-
+	router.PathPrefix("/static/").Handler(http.FileServer(ui.Assets)).Schemes("https")
+	router.PathPrefix("/").Schemes("http").HandlerFunc(httpsRedirectHandler)
 	return router
 }
