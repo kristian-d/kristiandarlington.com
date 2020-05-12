f533e4a9115abf76303f0d74b902dc9cd6aee326
Kristian Darlington <kristian.f.darlington@gmail.com>
2020-05-12 16:37:38 -0700

    added logging to route handlerFuncs

--- web/handlers.go
+++ web/handlers.go
@@ -2,6 +2,8 @@ package web
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/google/logger"
 	"github.com/kristian-d/kristiandarlington.com/web/ui"
 	"html/template"
 	"io/ioutil"
@@ -40,13 +42,15 @@ func getTemplate(templateName string) (string, error) {
 }
 
 func (h *handler) index(w http.ResponseWriter, r *http.Request) {
-	rawTmpl, err := getTemplate( "index.html")
+	rawTmpl, err := getTemplate("index.html")
 	if err != nil {
+		h.logger.Error("failed to get template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	tmpl, err := template.New("").Parse(rawTmpl)
 	if err != nil {
+		h.logger.Error("failed to parse template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -56,11 +60,13 @@ func (h *handler) index(w http.ResponseWriter, r *http.Request) {
 func (h *handler) projects(w http.ResponseWriter, r *http.Request) {
 	rawTmpl, err := getTemplate("projects.html")
 	if err != nil {
+		h.logger.Error("failed to get template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	tmpl, err := template.New("").Parse(rawTmpl)
 	if err != nil {
+		h.logger.Error("failed to parse template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -70,11 +76,13 @@ func (h *handler) projects(w http.ResponseWriter, r *http.Request) {
 func (h *handler) resume(w http.ResponseWriter, r *http.Request) {
 	rawTmpl, err := getTemplate("resume.html")
 	if err != nil {
+		h.logger.Error("failed to get template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	tmpl, err := template.New("").Parse(rawTmpl)
 	if err != nil {
+		h.logger.Error("failed to parse template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -84,11 +92,13 @@ func (h *handler) resume(w http.ResponseWriter, r *http.Request) {
 func (h *handler) about(w http.ResponseWriter, r *http.Request) {
 	rawTmpl, err := getTemplate("about.html")
 	if err != nil {
+		h.logger.Error("failed to get template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	tmpl, err := template.New("").Parse(rawTmpl)
 	if err != nil {
+		h.logger.Error("failed to parse template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -98,11 +108,13 @@ func (h *handler) about(w http.ResponseWriter, r *http.Request) {
 func (h *handler) contact(w http.ResponseWriter, r *http.Request) {
 	rawTmpl, err := getTemplate("contact.html")
 	if err != nil {
+		h.logger.Error("failed to get template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	tmpl, err := template.New("").Parse(rawTmpl)
 	if err != nil {
+		h.logger.Error("failed to parse template err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -111,6 +123,7 @@ func (h *handler) contact(w http.ResponseWriter, r *http.Request) {
 
 func (h *handler) contactSend(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm(); if err != nil {
+		h.logger.Error(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -121,6 +134,7 @@ func (h *handler) contactSend(w http.ResponseWriter, r *http.Request) {
 	validation, recaptchaErr := http.PostForm("https://www.google.com/recaptcha/api/siteverify",
 		url.Values{ "secret": {h.cfg.RecaptchaSecretKey}, "response": {recaptchaResponse}})
 	if recaptchaErr != nil {
+		h.logger.Error("failed to post recaptcha form to external server err=", recaptchaErr)
 		http.Error(w, recaptchaErr.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -139,16 +153,19 @@ func (h *handler) contactSend(w http.ResponseWriter, r *http.Request) {
 
 	validationBytes, validationErr := ioutil.ReadAll(validation.Body)
 	if validationErr != nil {
+		h.logger.Error("failed to read recaptcha response err=", validationErr)
 		http.Error(w, validationErr.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	err = json.Unmarshal(validationBytes, &v); if err != nil {
+		h.logger.Error("failed to unmarshal recaptcha response err=", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if v.success == false {
+		h.logger.Info(fmt.Sprintf("recaptcha failed verification res=%+v", v))
 		http.Error(w, "recaptcha failed verification", http.StatusInternalServerError)
 		return
 	}
@@ -165,6 +182,7 @@ func (h *handler) contactSend(w http.ResponseWriter, r *http.Request) {
 		[]string{h.cfg.EmailCredentials.Address},
 		[]byte(content))
 	if err != nil {
+		logger.Error("failed to send mail to contact email err=", err, "\tcontent=", content)
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
